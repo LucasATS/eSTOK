eaf805bf84c3421a7ce723c991c0ea974a4dd9f9
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-05 11:22:18 -0400

    criando tela Home

---
+++ FRONT-DEV/admin-web/src/modules/home/index.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col w-full h-full sm:p-20">
+      <div className="flex flex-col gap-20 w-full h-full text-center items-center justify-center">
+        <span className="sm:text-3xl text-2xl">Bem vindo a área do colaborador!</span>
+        {/* <CollaboratorsPhoto className="lg:w-[560px] sm:w-96 w-80 h-auto mx-auto" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
