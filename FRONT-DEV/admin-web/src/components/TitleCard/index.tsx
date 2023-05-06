dc1369f1feb0eb2cc0799a294563e51361596e10
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-05 21:36:04 -0400

    corrigindo tamanho título do componente Header

--- FRONT-DEV/admin-web/src/components/TitleCard/index.tsx
+++ FRONT-DEV/admin-web/src/components/TitleCard/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 const TitleCard: React.FC<Props> = ({ text }) => {
   return (
     <div className="sm:py-5 py-3 items-center">
-      <span className="font-bold text-3xl/3 text-secondary">{text}</span>
+      <span className="font-bold text-2xl/[10px] text-secondary">{text}</span>
     </div>
   );
 };
