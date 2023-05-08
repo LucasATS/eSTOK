80f69f55c5bfc6818cf14c0fdcb745a59dcf0c58
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-08 16:07:35 -0400

    criação do componente SwitchForm

---
+++ FRONT-DEV/admin-web/src/components/FormComponents/SwitchForm.tsx
@@ -0,0 +1,37 @@
+interface ConfigModalProps {
+  text: string;
+  onChange: (value: boolean) => void;
+  value: boolean;
+}
+
+const SwitchForm = ({ text, onChange, value }: ConfigModalProps) => {
+  const handleClick = () => {
+    onChange(!value);
+  };
+
+  return (
+    <div className="flex flex-row w-full items-center ">
+      <span className="whitespace-nowrap border-y-0">{text}</span>
+      <div
+        className="md:w-14 md:h-7 w-12 h-6 flex items-center rounded-full p-1 cursor-pointer"
+        onClick={handleClick}
+      >
+        <div
+          className={`${
+            value ? 'bg-teal-600' : 'bg-gray-600'
+          } inline-flex h-4 w-10 items-center rounded-full`}
+          onChange={handleClick}
+        >
+          <span
+            className={`${
+              value ? 'translate-x-[26px]' : 'translate-x-0.5'
+            } inline-block h-3 w-3 transform rounded-full bg-white transition`}
+            onChange={handleClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SwitchForm;
