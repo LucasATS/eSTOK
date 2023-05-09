56ab559db7348337892ce1d4d85d4594bde6299f
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-09 17:23:29 -0400

    corrigindo listagem de produto

--- FRONT-DEV/admin-web/src/modules/produto/pages/ListProduct/index.tsx
+++ FRONT-DEV/admin-web/src/modules/produto/pages/ListProduct/index.tsx
@@ -1,15 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '../../../../components/Button';
 import Pagination from '../../../../components/Paginate';
+import TitleCard from '../../../../components/TitleCard';
+import { PaginateDto } from '../../../_shared/dto/PaginateDto';
 import { Paginate } from '../../../_shared/types/api.types';
 import Produto from '../../models/Produto';
-import CreateProduct from './components/CreateProduct';
+import { NewProductModal } from './components/NewProductModal';
 import ProductTable from './components/ProductTable/index';
 
 const ListProduct = () => {
   const [productIdActive, setProductIdActive] = useState<number>();
+  const [paginationActive, setPaginationActive] = useState<PaginateDto>({});
+
   const [productsPaginate, setProductsPaginate] = useState<Paginate<Produto>>();
+  const [openNewProductModal, setOpenNewProductModal] = useState(false);
   const navigate = useNavigate();
+
+  const loadProduct = async () => {
+    // setProductsPaginate();
+  };
+
+  const handleNewProduct = () => {
+    loadProduct();
+  };
+
+  const handleClickNewProduct = () => {
+    setOpenNewProductModal(true);
+  };
+
+  const handleCloseNewProduct = () => {
+    setOpenNewProductModal(false);
+  };
 
   const handleClickDeleteProduct = (id: number) => {
     setProductIdActive(id);
@@ -20,21 +42,32 @@ const ListProduct = () => {
   };
 
   const onChangePage = async (page: number) => {
-    // setProductsPaginate((old) => ({ ...old, page }));
+    setPaginationActive((old) => ({ ...old, page }));
+    console.log('Próxima página');
   };
 
+  useEffect(() => {
+    loadProduct();
+  }, [paginationActive]);
+
   return (
-    <div className="flex flex-col mx-8">
-      <div className="flex bg-white mt-6 rounded-[30px] p-6 w-full">
-        <CreateProduct />
+    <div className="flex flex-col mx-8 bg-white mt-6 rounded-[30px] p-5">
+      <div className="flex flex-wrap justify-between items-center">
+        <TitleCard text="Produto" />
+        <div className="flex items-center sm:py-5 py-3 gap-3 w-full sm:w-auto justify-end">
+          <Button
+            buttonText="Novo"
+            variant="primary"
+            type="button"
+            onClick={handleClickNewProduct}
+          />
+        </div>
       </div>
-      <div className="flex bg-white mt-6 rounded-[30px] p-6 w-full">
+      <div className="flex flex-col gap-2 mt-5">
         <ProductTable
           onClickEditProduto={handleClickEditProduct}
           onClickDeleteProduto={() => handleClickDeleteProduct}
         />
-      </div>
-      <div>
         <Pagination
           currentPage={productsPaginate?.results.length}
           page={productsPaginate?.currentPage}
@@ -43,6 +76,11 @@ const ListProduct = () => {
           onChangePage={onChangePage}
         />
       </div>
+      <NewProductModal
+        isOpen={openNewProductModal}
+        onClose={handleCloseNewProduct}
+        onConfirm={handleNewProduct}
+      />
     </div>
   );
 };
