beb318e0a113967fc5bf7c3b2df5a2389ea47078
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-09 17:22:10 -0400

    criando ModalComponent

--- FRONT-DEV/admin-web/src/components/MainLayout/index.tsx
+++ FRONT-DEV/admin-web/src/components/MainLayout/index.tsx
@@ -64,7 +64,7 @@ const MainLayout: React.FC<Props> = ({ children }) => {
                 </Link>
 
                 <Link to={RoutesURL.LIST_PRODUCT}>
-                  <div className="px-4 hover:font-semibold py-2 flex gap-2 items-center cursor-pointer w-full font-medium text-base hover:bg-sky-700 hover:rounded-r-[20px] transition-all ease-in-out rounded-sm">
+                  <div className="px-4 py-2 flex gap-2 items-center cursor-pointer w-full font-medium text-base hover:bg-sky-700 hover:rounded-r-[20px] transition-all ease-in-out rounded-sm">
                     <img src={iconApi + 'product.svg'} alt="product" className="w-6 h-6" />
                     Produto
                   </div>
