b107424dc7953e668497cd87f88116de69b1390d
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-11 19:02:18 -0400

    adicionando componente Dropdown nos componentes ListCard, Table e ListProduct

--- FRONT-DEV/admin-web/src/modules/produto/pages/ListProduct/index.tsx
+++ FRONT-DEV/admin-web/src/modules/produto/pages/ListProduct/index.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../../../components/Button';
+import Dropdown from '../../../../components/Dropdown';
 import Pagination from '../../../../components/Paginate';
+import { Action } from '../../../../components/Table';
 import TitleCard from '../../../../components/TitleCard';
+import RoutesURL from '../../../_shared/constants/RoutesURL.enum';
 import { PaginateDto } from '../../../_shared/dto/PaginateDto';
 import { Paginate } from '../../../_shared/types/api.types';
 import Produto from '../../models/Produto';
@@ -12,10 +15,16 @@ import ProductTable from './components/ProductTable/index';
 const ListProduct = () => {
   const [productIdActive, setProductIdActive] = useState<number>();
   const [paginationActive, setPaginationActive] = useState<PaginateDto>({});
-
   const [productsPaginate, setProductsPaginate] = useState<Paginate<Produto>>();
   const [openNewProductModal, setOpenNewProductModal] = useState(false);
   const navigate = useNavigate();
+
+  const menuItens: Action[] = [
+    { label: 'Categoria', onClick: () => navigate(RoutesURL.HOME) },
+    { label: 'Tipo de Produto', onClick: () => navigate(RoutesURL.HOME) },
+    { label: 'Unidade de Medida', onClick: () => navigate(RoutesURL.HOME) },
+    { label: 'Tamanho de Produto', onClick: () => navigate(RoutesURL.HOME) }
+  ];
 
   const loadProduct = async () => {
     // setProductsPaginate();
@@ -54,7 +63,8 @@ const ListProduct = () => {
     <div className="flex flex-col mx-8 bg-white mt-6 rounded-[30px] p-5">
       <div className="flex flex-wrap justify-between items-center">
         <TitleCard text="Produto" />
-        <div className="flex md:px-4 w-auto justify-end">
+        <div className="flex md:px-4 w-auto gap-3 justify-end">
+          <Dropdown menuItens={menuItens} text="Cadastros" variant="clean" />
           <Button
             buttonText="Novo"
             variant="primary"
