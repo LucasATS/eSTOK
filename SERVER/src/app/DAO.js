8d5cb066086329c75b59a32776f46d1002e81f76
Thales Tayson do Nascimento Vargas <tvargas@FUNSAU.MS>
2023-05-10 21:52:43 -0400

    clear

--- SERVER/src/app/DAO.js
+++ SERVER/src/app/DAO.js
@@ -48,21 +48,6 @@ const adicionarDadosTest = async (models) => {
 }
 
 
-/**
-cpf: { type: DataTypes.STRING(11), allowNull: false, unique: true },
-      nome: { type: DataTypes.STRING(127), allowNull: false },
-      login: { type: DataTypes.STRING(25), allowNull: false, unique: true },
-      email: { type: DataTypes.STRING(127), allowNull: false, unique: true },
-      senha_reset: { type: DataTypes.BOOLEAN(), allowNull: false, defaultValue: true },
-      senha: { type: DataTypes.STRING(127), allowNull: false },
-      id_status: {
-        type: DataTypes.INTEGER, references: {
-          model: Status,
-          key: 'id'
-        }
-      },
- */
-
 const criarStatus = async (
     Status, {
         descricao = '',
