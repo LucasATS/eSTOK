940554cb0882e2f62759411795f05ed92d782fa3
MATHEUS OLIVEIRA <matheusoliver_123@hotmail.com>
2023-05-12 00:21:20 -0400

    Create CSS modal do cadastro da unidade de medida

--- FRONT-DEV/admin-web/src/modules/unidMedida/components/NewUnidMedidaModal.tsx
+++ FRONT-DEV/admin-web/src/modules/unidMedida/components/NewUnidMedidaModal.tsx
@@ -31,14 +31,15 @@ const NewUnidMedidaModal = ({ isOpen, onClose, onConfirm }: ConfigModalProps) =>
   return (
     <ModalComponent isOpen={isOpen} onClose={onClose}>
       <Form ref={formRef} onSubmit={handleAddNewUnidMedida}>
-        <div>
-          <div>
+        <div className="relative bg-white rounded-lg shadow w-full">
+          <div className="flex items-start py-1 px-4 rounded-t border-b">
             <TitleCard text="Cadastrar Unidade de Medida" />
           </div>
-          <div>
+          <div className="flex-col mx-4">
             <InputForm name="nomeUnidade" type="text" placeholder="Unidade de Medida" />
           </div>
-          <div>
+          <br></br>
+          <div className="flex md:px-4 justify-between py-4 px-1">
             <Button variant="cancel" type="button" onClick={handleCancel}>
               Cancelar
             </Button>
--- FRONT-DEV/admin-web/src/modules/unidMedida/index.tsx
+++ FRONT-DEV/admin-web/src/modules/unidMedida/index.tsx
@@ -15,13 +15,19 @@ const UnidMedida = () => {
   };
 
   return (
-    <div className="">
-      <div>
+    <div className="flex flex-col mx-8 bg-white mt-6 rounded-[30px] p-5">
+      <div className="flex flex-wrap justify-between items-center">
         <TitleCard text="Unidade de Medida" />
+        <div className="flex md:px-4 w-auto justify-end">
+          <Button
+            buttonText="Novo"
+            variant="primary"
+            type="button"
+            onClick={handleClickNewUnidMedida}
+          />
+        </div>
       </div>
-      <div>
-        <Button buttonText="Novo" onClick={handleClickNewUnidMedida} />
-      </div>
+
       <NewUnidMedidaModal
         onClose={handleCloseNewUnidMedida}
         isOpen={openNewUnidMedidaModal}
