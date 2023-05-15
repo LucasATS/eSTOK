ea7acb8c6a296cef829f7b36d8c8f89429085458
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-15 17:40:21 -0400

    excluindo helpers ClearForm

--- FRONT-DEV/admin-web/src/modules/_shared/helpers/ClearForm.ts
+++
@@ -1,8 +0,0 @@
-import { FormHandles } from '@unform/core';
-import { useRef } from 'react';
-
-const formRef = useRef<FormHandles>(null);
-
-export const clearForm = () => {
-  formRef.current?.reset();
-};
