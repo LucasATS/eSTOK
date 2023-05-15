a1b3e4cdaf550dbf80793d9f6e692508e6906817
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-15 18:14:28 -0400

    corrigindo mainLayout

--- FRONT-DEV/admin-web/src/components/MainLayout/index.tsx
+++ FRONT-DEV/admin-web/src/components/MainLayout/index.tsx
@@ -5,7 +5,6 @@ import { Link, useLocation } from 'react-router-dom';
 import RoutesURL from '../../modules/_shared/constants/RoutesURL.enum';
 import iconApi from '../../modules/_shared/services/iconApi';
 import { useAuth } from '../../modules/auth/contexts/AuthProvider';
-import Header from './components/Header';
 
 interface Props {
   children: React.ReactNode;
@@ -42,21 +41,17 @@ const MainLayout: React.FC<Props> = ({ children }) => {
           registrationPage ? 'max-h-screen h-auto lg:bg-default' : 'h-screen'
         } `}
       >
-        <div className="text-white w-full md:w-56 lg:w-72 md:h-full bg-sky-600 rounded-r-[30px] shadow-lg md:fixed z-50 md:z-0">
+        <div className="text-white w-full md:w-56 lg:w-72 md:h-full bg-sky-600 rounded-br-[40px] shadow-lg md:fixed z-50 md:z-0">
           <div className="flex flex-row md:flex-col items-center md:items-start">
             <div className="md:flex flex-col hidden md:w-full mt-14 gap-7">
               <div className="flex justify-center">
                 <img src={iconApi + 'e-stok.svg'} alt="logo" />
-              </div>
-              <div className="flex justify-center">
-                <img src={iconApi + 'mask-group.svg'} alt="user-picture" />
               </div>
               <div className="flex items-center justify-center">
                 <span className="capitalize font-medium">
                   {/* {user?.name.split(' ').shift()?.toLocaleLowerCase()} */} Leonardo vieira
                 </span>
               </div>
-              {/* ROTAS DO MENU */}
               <div className="flex flex-col mr-10">
                 <Link to={RoutesURL.HOME}>
                   <div className="px-4 py-2 flex gap-2 items-center cursor-pointer w-full font-medium text-base hover:bg-sky-700 hover:rounded-r-[20px] transition-all ease-in-out rounded-sm">
@@ -87,9 +82,6 @@ const MainLayout: React.FC<Props> = ({ children }) => {
           </div>
         </div>
         <div className="flex flex-col bg-neutral-200 overflow-y-auto">
-          <div className="bg-white">
-            <Header />
-          </div>
           <div className="flex flex-col lg:ml-72 md:ml-56 h-screen">{children}</div>
         </div>
       </div>
--- FRONT-DEV/admin-web/src/modules/produto/pages/ListProduct/index.tsx
+++ FRONT-DEV/admin-web/src/modules/produto/pages/ListProduct/index.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../../../components/Button';
 import Dropdown from '../../../../components/Dropdown';
+import Header from '../../../../components/MainLayout/components/Header';
 import Pagination from '../../../../components/Paginate';
 import { Action } from '../../../../components/Table';
-import TitleCard from '../../../../components/TitleCard';
 import RoutesURL from '../../../_shared/constants/RoutesURL.enum';
 import { PaginateDto } from '../../../_shared/dto/PaginateDto';
 import { Paginate } from '../../../_shared/types/api.types';
@@ -60,10 +60,13 @@ const ListProduct = () => {
   }, [paginationActive]);
 
   return (
-    <div className="flex flex-col mx-8 bg-white mt-6 rounded-[30px] p-5">
-      <div className="flex flex-wrap justify-between items-center">
-        <TitleCard text="Produto" />
-        <div className="flex md:px-4 w-auto gap-3 justify-end">
+    <div className="w-full flex flex-col">
+      <div className="w-full bg-white justify-start items-start">
+        <Header mainText="Produto" />
+      </div>
+      <div className="flex flex-col mx-8 bg-white mt-6 rounded-[30px] p-5">
+        {/* <div className="flex flex-wrap justify-between items-center"> */}
+        <div className="flex flex-row md:px-4 w-auto gap-3 justify-end items-end">
           <Dropdown menuItens={menuItens} text="Cadastros" variant="clean" />
           <Button
             buttonText="Novo"
@@ -72,25 +75,26 @@ const ListProduct = () => {
             onClick={handleClickNewProduct}
           />
         </div>
-      </div>
-      <div className="flex flex-col gap-2 mt-5">
-        <ProductTable
-          onClickEditProduto={handleClickEditProduct}
-          onClickDeleteProduto={() => handleClickDeleteProduct}
+        {/* </div> */}
+        <div className="flex flex-col gap-2 mt-5">
+          <ProductTable
+            onClickEditProduto={handleClickEditProduct}
+            onClickDeleteProduto={() => handleClickDeleteProduct}
+          />
+          <Pagination
+            currentPage={productsPaginate?.results.length}
+            page={productsPaginate?.currentPage}
+            pageSize={productsPaginate?.limit}
+            totalItems={productsPaginate?.totalItems}
+            onChangePage={onChangePage}
+          />
+        </div>
+        <NewProductModal
+          isOpen={openNewProductModal}
+          onClose={handleCloseNewProduct}
+          onConfirm={handleNewProduct}
         />
-        <Pagination
-          currentPage={productsPaginate?.results.length}
-          page={productsPaginate?.currentPage}
-          pageSize={productsPaginate?.limit}
-          totalItems={productsPaginate?.totalItems}
-          onChangePage={onChangePage}
-        />
       </div>
-      <NewProductModal
-        isOpen={openNewProductModal}
-        onClose={handleCloseNewProduct}
-        onConfirm={handleNewProduct}
-      />
     </div>
   );
 };
