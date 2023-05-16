c0816e9a100f79e9759dec8a6cbfdd6f72245651
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-16 18:59:31 -0400

    alterando cor componente TitleCard

--- FRONT-DEV/portal-web/src/components/TitleCard/index.tsx
+++ FRONT-DEV/portal-web/src/components/TitleCard/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 const TitleCard: React.FC<Props> = ({ text }) => {
   return (
     <div className="sm:py-5 py-3 items-center">
-      <span className="font-bold text-2xl/[10px] text-secondary">{text}</span>
+      <span className="font-bold text-2xl/[10px] text-primary">{text}</span>
     </div>
   );
 };
