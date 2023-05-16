d775143bd29ddadb18b993a188874b9b8ebf568f
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-16 18:58:08 -0400

    excluindo rota Product

--- FRONT-DEV/portal-web/src/routes/AppRoutes.tsx
+++ FRONT-DEV/portal-web/src/routes/AppRoutes.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { MainLayout } from '../components/MainLayout';
-import RoutesStore from '../modules/_shared/constants/RoutesStore.enum';
 import { Home } from '../modules/home/pages';
-import { Product } from '../modules/product/pages';
 
 const AppRoutes: React.FC = () => {
   return (
     <HashRouter>
       <MainLayout>
         <Routes>
-          <Route index path={RoutesStore.HOME} element={<Home />} />
-          <Route path={RoutesStore.PRODUCT} element={<Product />} />
+          <Route index path="/" element={<Home />} />
         </Routes>
       </MainLayout>
     </HashRouter>
