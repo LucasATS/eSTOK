825173225d10885605e710a4f1b3fbf2b293fb2f
Lucas Almeida Tiburtino da Silva <64665422+LucasATS@users.noreply.github.com>
2023-05-17 10:17:05 -0400

    pequenos ajustes

--- FRONT-DEV/portal-web/src/index.tsx
+++ FRONT-DEV/portal-web/src/index.tsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('portal-root') as HTMLElement);
 root.render(<App />);
