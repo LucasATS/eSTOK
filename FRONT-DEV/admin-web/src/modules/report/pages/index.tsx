6a76d8256162a04e44aa522cdf261eeb6f582248
Matheus Figueiredo <101054993+MatheusFiigueiredo@users.noreply.github.com>
2023-05-22 20:56:11 -0400

    Atualização gerar relatório

--- FRONT-DEV/admin-web/src/modules/report/pages/index.tsx
+++ FRONT-DEV/admin-web/src/modules/report/pages/index.tsx
@@ -13,51 +13,53 @@ import {
 
 export const CreateReport = () => {
   const formRef = useRef<FormHandles>(null);
+
   const handleAddNewAggregatedHolerite = async () => {
     console.log('criado ou atualizado');
     clearForm();
   };
+
   const clearForm = () => {
     formRef.current?.reset();
   };
+
   return (
     <div className="w-full flex flex-col">
       <div className="w-full bg-white justify-start items-start">
         <Header mainText="Relatório" />
       </div>
       <div className="flex flex-col mx-8 bg-white mt-6 rounded-[30px] p-5">
-        <Form ref={formRef} onSubmit={handleAddNewAggregatedHolerite}>
-          <div>
-            <div>
-              <TitleCard text="Gerar relatório" />
-            </div>
-            <div>
-              <SelectForm
-                name="productType"
-                placeholder="Selecione o tipo de relatório"
-                options={selectOptionsReportType}
-              />
-              <SelectForm
-                name="productType"
-                placeholder="Selecione o produto"
-                options={selectOptionsProductType}
-              />
-              <SelectForm
-                name="productType"
-                placeholder="Selecione o período"
-                options={selectOptionsPeriodType}
-              />
-            </div>
-            <div>
-              <Button
-                variant="primary"
-                type="button"
-                onClick={handleAddNewAggregatedHolerite}
-                buttonText="Gerar"
-              />
-            </div>
+        <TitleCard text="Gerar Relatório" />
+        <Form
+          ref={formRef}
+          onSubmit={handleAddNewAggregatedHolerite}
+          className="flex flex-col gap-6"
+        >
+          <div className="flex flex-row gap-2">
+            <SelectForm
+              name="productType"
+              placeholder="Tipo de Relatório"
+              options={selectOptionsReportType}
+            />
+            <SelectForm
+              name="productType"
+              placeholder="Produto"
+              options={selectOptionsProductType}
+            />
+            <SelectForm
+              name="productType"
+              placeholder="Período"
+              options={selectOptionsPeriodType}
+            />
           </div>
-          <div></div>
+          <div className="flex justify-end">
+            <Button
+              variant="primary"
+              type="button"
+              onClick={handleAddNewAggregatedHolerite}
+              buttonText="Gerar Relatório"
+            />
+          </div>
         </Form>
       </div>
     </div>
