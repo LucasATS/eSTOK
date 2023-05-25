94f1d80ee308abd7d28a0e6fe4f3ed70ef60b025
2LordDark2 <klebersonalans@gmail.com>
2023-05-24 22:22:43 -0300

    atualizacao de model

--- SERVER/src/models/modelKardexTipo.js
+++ SERVER/src/models/modelKardexTipo.js
@@ -4,3 +4,5 @@ import db from '../settings/db';
  export const KardexTipo = db.define('kardex_tipo', {
     descricao: { type: DataTypes.STRING(50), allowNull: false },
 }, {});
+
+export default KardexTipo
