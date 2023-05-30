89e50f467022fcaec05a1c5609062b97eb0e471d
Sérgio Adriano Sousa Santos <105815527+SergioAdrianoSousa@users.noreply.github.com>
2023-05-29 21:00:31 -0400

    Alterações na viewProdutos

--- SERVER/src/views-admin/viewProdutos.js
+++ SERVER/src/views-admin/viewProdutos.js
@@ -6,7 +6,7 @@ const view = async (req, res) => {
     const data = await DAO.filter(
         modelProduto,
         {},
-        ['Nome', 'Descricao', 'Foto']
+        ['Nome', 'Descricao', 'Foto', 'id_id_unidade', 'id_categoria', 'id_tp_produto', 'fungibilidade', 'estocavel', 'id_status']
     );
 
     res.status(200).json({ data: data });
