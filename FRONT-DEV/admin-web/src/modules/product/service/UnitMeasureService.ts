3fdd5a77a542f7fcd4e87806b2eac0be9eb37f8b
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-06-17 21:08:32 -0400

    integração unidade de medida

--- FRONT-DEV/admin-web/src/modules/product/service/UnitMeasureService.ts
+++ FRONT-DEV/admin-web/src/modules/product/service/UnitMeasureService.ts
@@ -1,17 +1,4 @@
-import api from '../../_shared/services/api';
-import { Result } from '../../_shared/types/api.types';
-import CreateUnitMeasureDto from '../dto/CreateUnitMeasureDto';
-import ResultUnitMeasureDto from '../dto/ResultUnitMeasureDto';
-
 class UnitMeasureService {
-  public async createMeasure(
-    unitMeasuDto: CreateUnitMeasureDto
-  ): Promise<Result<ResultUnitMeasureDto>> {
-    console.log(unitMeasuDto);
-    const response = await api.post(`/api/admin/tipos-de-medidas/create`, unitMeasuDto);
-    return response.data;
-  }
-
   // public async listMeasure(
   //   listUnitMeasure: ListUnitMeasureDto
   // ): Promise<Result<ResultUnitMeasureDto>> {
