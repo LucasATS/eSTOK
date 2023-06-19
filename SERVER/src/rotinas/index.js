5323a86781e0a15c310dec5d4ef2889b22047aa6
unknown <taysonvargas@hotmail.com>
2023-06-19 11:33:37 -0400

    correcao tpProdutos

--- SERVER/src/rotinas/index.js
+++ SERVER/src/rotinas/index.js
@@ -2,7 +2,7 @@ import { schedule } from 'node-cron';
 import db from '../settings/db';
 
 export const rotina_inativa_lotes = () => {
-    schedule('0 0 * * *', () => {
+    schedule('0 0 */5 * *', () => {
         //6h em 6h
         db.query("CALL sp_inativa_lotes();");
     });
