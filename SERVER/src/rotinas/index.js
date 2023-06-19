953eb1207acb010d705f07a316ea1f2cb29b726c
Thales Tayson do Nascimento Vargas <tvargas@FUNSAU.MS>
2023-06-19 04:14:56 -0400

    rotina sql

--- SERVER/src/rotinas/index.js
+++ SERVER/src/rotinas/index.js
@@ -2,8 +2,8 @@ import { schedule } from 'node-cron';
 import db from '../settings/db';
 
 export const rotina_inativa_lotes = () => {
-    // 6 em 6h
-    schedule('0 0 */5 * *', () => {
-        console.log("TESTE")
+    schedule('0 0 * * *', () => {
+        //6h em 6h
+        db.query("CALL sp_inativa_lotes();");
     });
 }
