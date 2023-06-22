45b13c0593e2d38fff1aad450992202ac831a6ef
Thales Tayson do Nascimento Vargas <tvargas@FUNSAU.MS>
2023-06-22 18:16:12 -0400

    updates

--- SERVER/src/forms/formProduto.js
+++ SERVER/src/forms/formProduto.js
@@ -16,6 +16,10 @@ const FormularioProduto = async (body) => {
     let fungibilidade_e = eval(fungibilidade) ? 1 : 0;
     let estocavel_e = eval(estocavel) ? 1 : 0;
 
+    if ( !foto ){
+        foto = '';
+    }
+    
     const resp = await Produtos.sp_produtos(nome, descricao, id_categoria, id_tp_produto, id_unidade, foto, fungibilidade_e, estocavel_e)
     
     return { is_valid: true, message: resp.Msg }
