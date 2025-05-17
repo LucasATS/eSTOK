b935b9bcdd7a7013b6ea1b7de47be0f7471d782b
bamarcheti <bmarchetifiorin@gmail.com>
2025-05-17 15:43:15 -0400

    fix[admin-web] Ajustando responsividade Lista e Cadastro Estoque e Baixa Estoque

--- FRONT-DEV/admin-web/src/modules/stock/pages/components/NewStockModal.tsx
+++ FRONT-DEV/admin-web/src/modules/stock/pages/components/NewStockModal.tsx
@@ -144,9 +144,9 @@ export const NewStockModal = ({ isOpen, onClose, onConfirm }: ConfigModalProps) 
               </div>
             </div>
 
-            <div className="flex items-center justify-end p-6 space-x-3 rounded-b border-t border-gray-200">
+            <div className="flex flex-col md:flex-row items-center justify-end p-6 gap-3 rounded-b border-t border-gray-200">
               <Button
-                style={{ width: '200px' }}
+                className=" w-full md:w-52"
                 type="button"
                 variant="cancel"
                 onClick={handleCancel}
@@ -154,7 +154,7 @@ export const NewStockModal = ({ isOpen, onClose, onConfirm }: ConfigModalProps) 
                 Cancelar
               </Button>
               <Button
-                style={{ width: '200px' }}
+                className=" w-full md:w-52"
                 variant="primary"
                 type="submit"
                 buttonText="Cadastrar"
